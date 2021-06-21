12783e83c618007df128df6952ebc9c991c413d1
whuan1024 <whuan1024@gmail.com>
2021-06-21 17:42:19 +0800

    Remove log4j
    Change-Id: Id3a2c12f7db4a0aee01524068efe87f8767ecfcf

--- VendingSdk/src/main/java/com/cloudminds/vending/utils/LogUtil.java
+++ VendingSdk/src/main/java/com/cloudminds/vending/utils/LogUtil.java
@@ -1,6 +1,7 @@
 package com.cloudminds.vending.utils;
 
-import org.apache.log4j.Logger;
+
+import android.util.Log;
 
 public class LogUtil {
 
@@ -15,33 +16,39 @@ public class LogUtil {
     //==========================================================
 
     public static void v(String msg) {
-        Logger LOGGER = Logger.getLogger(TAG);
-        LOGGER.info(msg);
+//        Logger LOGGER = Logger.getLogger(TAG);
+//        LOGGER.info(msg);
+        Log.v(TAG, msg);
     }
 
     public static void d(String msg) {
-        Logger LOGGER = Logger.getLogger(TAG);
-        LOGGER.debug(msg);
+//        Logger LOGGER = Logger.getLogger(TAG);
+//        LOGGER.debug(msg);
+        Log.d(TAG, msg);
     }
 
     public static void i(String msg) {
-        Logger LOGGER = Logger.getLogger(TAG);
-        LOGGER.info(msg);
+//        Logger LOGGER = Logger.getLogger(TAG);
+//        LOGGER.info(msg);
+        Log.i(TAG, msg);
     }
 
     public static void w(String msg) {
-        Logger LOGGER = Logger.getLogger(TAG);
-        LOGGER.warn(msg);
+//        Logger LOGGER = Logger.getLogger(TAG);
+//        LOGGER.warn(msg);
+        Log.w(TAG, msg);
     }
 
     public static void e(String msg) {
-        Logger LOGGER = Logger.getLogger(TAG);
-        LOGGER.error(msg);
+//        Logger LOGGER = Logger.getLogger(TAG);
+//        LOGGER.error(msg);
+        Log.e(TAG, msg);
     }
 
     public static void e(String msg, Throwable tr) {
-        Logger LOGGER = Logger.getLogger(TAG);
-        LOGGER.error(msg, tr);
+//        Logger LOGGER = Logger.getLogger(TAG);
+//        LOGGER.error(msg, tr);
+        Log.e(TAG, msg, tr);
     }
 
     //==========================================================
@@ -49,32 +56,38 @@ public class LogUtil {
     //==========================================================
 
     public static void v(String tag, String msg) {
-        Logger LOGGER = Logger.getLogger(tag);
-        LOGGER.info(msg);
+//        Logger LOGGER = Logger.getLogger(tag);
+//        LOGGER.info(msg);
+        Log.v(tag, msg);
     }
 
     public static void d(String tag, String msg) {
-        Logger LOGGER = Logger.getLogger(tag);
-        LOGGER.debug(msg);
+//        Logger LOGGER = Logger.getLogger(tag);
+//        LOGGER.debug(msg);
+        Log.d(tag, msg);
     }
 
     public static void i(String tag, String msg) {
-        Logger LOGGER = Logger.getLogger(tag);
-        LOGGER.info(msg);
+//        Logger LOGGER = Logger.getLogger(tag);
+//        LOGGER.info(msg);
+        Log.i(tag, msg);
     }
 
     public static void w(String tag, String msg) {
-        Logger LOGGER = Logger.getLogger(tag);
-        LOGGER.warn(msg);
+//        Logger LOGGER = Logger.getLogger(tag);
+//        LOGGER.warn(msg);
+        Log.w(tag, msg);
     }
 
     public static void e(String tag, String msg) {
-        Logger LOGGER = Logger.getLogger(tag);
-        LOGGER.error(msg);
+//        Logger LOGGER = Logger.getLogger(tag);
+//        LOGGER.error(msg);
+        Log.e(tag, msg);
     }
 
     public static void e(String tag, String msg, Throwable tr) {
-        Logger LOGGER = Logger.getLogger(tag);
-        LOGGER.error(msg, tr);
+//        Logger LOGGER = Logger.getLogger(tag);
+//        LOGGER.error(msg, tr);
+        Log.e(tag, msg, tr);
     }
 }
